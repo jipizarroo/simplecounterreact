d50449cc6f60818bce0d085fa10c28a1bae4c202
jipizarroo <jipizarroo@gmail.com>
2019-12-30 11:25:43 -0300

    here we go again

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+    // Add css files
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    //import 'font-awesome/css/font-awesome.min.css'; // 4.x
+    import '@fortawesome/fontawesome-free/css/all.min.css'; // 5.x
+
+
+    // Add js files
+    import 'jquery';
+    import 'popper.js';
+    import 'bootstrap';
